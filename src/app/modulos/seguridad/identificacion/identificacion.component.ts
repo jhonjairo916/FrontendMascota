30d7e840c0dd35b7f2dbb914735d88dcc4939f3d
jhonjairo916 <jhonjairo.916@hotmail.com>
2021-12-10 22:28:40 -0500

    Identificacion de usuario con error en backend, pendiente terminarlo

--- src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as cryptoJs from "crypto-js";
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 @Component({
   selector: 'app-identificacion',
   templateUrl: './identificacion.component.html',
   styleUrls: ['./identificacion.component.css']
 })
 export class IdentificacionComponent implements OnInit {
-
-  constructor() { }
+  //Se valida informacion y se genera comunicacion bidirecional entre frontend y backend
+  fgValidador: FormGroup = this.fb.group({
+    'usuario':['', [Validators.required, Validators.email]],
+    'clave': ['', [Validators.required]]
+  });
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService) { }
 
   ngOnInit(): void {
+  }
+   IdentificarUsuario(){
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+    //let claveCifrada = cryptoJs.MD5(clave).toString()
+    this.servicioSeguridad.identificar(usuario, clave).subscribe((datos:any)=>{
+      //ok
+      alert("datos Correctos");
+    },(error:any)=>{
+      alert("Datos invalidos.");
+    })
   }
 
 }
